16867ee313de29fa2eabd798e63ac1f43c020119
PaddyHuang <huangblcs@foxmail.com>
2020-09-25 10:33:35 +0800

    Update dockerfile for amd64 and arm64

--- ubuntu-cppenv/build.sh
+++ ubuntu-cppenv/build.sh
@@ -3,6 +3,16 @@ version=latest
 image="ubuntu-cppenv"
 repo="paddyace"
 
+#export DOCKER_CLI_EXPERIMENTAL=enabled
+#docker buildx create --use --name mybuilder
+#docker buildx ls
+
+#docker buildx build --platform linux/amd64 -f dockerfile -t ${repo}/${image}:amd64v1.0 .
+#docker push ${repo}/${image}:amd64v1.0
+
+#docker buildx build --platform linux/arm64 -f dockerfile -t ${repo}/${image}:arm64v1.0 .
+#docker push ${repo}/${image}:arm64v1.0
+
 docker build --rm -f dockerfile -t ${image}:${version} .
 docker tag ${image}:${version} ${repo}/${image}:${version} 
 docker push ${repo}/${image}:${version}
