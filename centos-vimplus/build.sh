54318ad5e8d0e68d0d0a798344f4157a3f451121
PaddyHuang <huangblcs@outlook.com>
2020-09-25 02:11:54 +0000

    Update centos-vimplus

--- centos-vimplus/build.sh
+++ centos-vimplus/build.sh
@@ -7,8 +7,11 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 #docker buildx create --use --name mybuilder
 docker buildx ls
 
-docker buildx build -f dockerfile -t ${repo}/${image}:amd64 .
-docker push ${repo}/${image}:amd64
+#docker buildx build --platform linux/amd64 -f dockerfile -t ${repo}/${image}:amd64 .
+#docker push ${repo}/${image}:amd64
+
+docker buildx build --platform linux/arm64 -f dockerfile -t ${repo}/${image}:arm64 .
+docker push ${repo}/${image}:arm64
 
 #docker build --rm -f dockerfile -t ${image}:${version} .
 #docker tag ${image}:${version} ${repo}/${image}:${version} 
