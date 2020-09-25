6ac1f4fae9120fcffa25d62ac837f31b6eb95af8
PaddyHuang <huangblcs@foxmail.com>
2020-09-25 10:37:39 +0800

    Update dockerfile for amd64 and arm64

--- centos-cppenv/build.sh
+++ centos-cppenv/build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 version=1.0
+amd64Ver=amd64v1.0
+arm64Ver=arm64v1.0
+
 image="centos-cppenv"
 repo="paddyace"
+
+#export DOCKER_CLI_EXPERIMENTAL=enabled
+#docker buildx create --use --name mybuilder
+#docker buildx ls
+
+#docker buildx build --platform linux/amd64 -f dockerfile -t ${repo}/${image}:amd64v1.0 .
+#docker push ${repo}/${image}:amd64v1.0
+
+#docker buildx build --platform linux/arm64 -f dockerfile -t ${repo}/${image}:arm64v1.0 .
+#docker push ${repo}/${image}:arm64v1.0
 
 docker build --rm -f dockerfile -t ${image}:${version} .
 docker tag ${image}:${version} ${repo}/${image}:${version} 
--- centos-vimplus/build.sh
+++ centos-vimplus/build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-version=test
+version=latest
+amd64Ver=amd64v1.0
+arm64Ver=arm64v1.0
+
 image="centos-vimplus"
 repo="paddyace"
 
-export DOCKER_CLI_EXPERIMENTAL=enabled
+#export DOCKER_CLI_EXPERIMENTAL=enabled
 #docker buildx create --use --name mybuilder
-docker buildx ls
+#docker buildx ls
 
 #docker buildx build --platform linux/amd64 -f dockerfile -t ${repo}/${image}:amd64 .
 #docker push ${repo}/${image}:amd64
 
-docker buildx build --platform linux/arm64 -f dockerfile -t ${repo}/${image}:arm64 .
-docker push ${repo}/${image}:arm64
+#docker buildx build --platform linux/arm64 -f dockerfile -t ${repo}/${image}:arm64 .
+#docker push ${repo}/${image}:arm64
 
-#docker build --rm -f dockerfile -t ${image}:${version} .
-#docker tag ${image}:${version} ${repo}/${image}:${version} 
-#docker push ${repo}/${image}:${version}
+docker build --rm -f dockerfile -t ${image}:${version} .
+docker tag ${image}:${version} ${repo}/${image}:${version} 
+docker push ${repo}/${image}:${version}
 if [ $? -eq 0 ]; then
  echo "push Success"
 else 
