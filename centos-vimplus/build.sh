f769f451928c9a91f3bfdd1e2fe6c04129aef587
PaddyHuang <huangblcs@foxmail.com>
2020-09-25 09:57:58 +0800

    Update centos-vimplus

--- centos-vimplus/build.sh
+++ centos-vimplus/build.sh
@@ -3,9 +3,16 @@ version=test
 image="centos-vimplus"
 repo="paddyace"
 
-docker build --rm -f dockerfile -t ${image}:${version} .
-docker tag ${image}:${version} ${repo}/${image}:${version} 
-docker push ${repo}/${image}:${version}
+export DOCKER_CLI_EXPERIMENTAL=enabled
+#docker buildx create --use --name mybuilder
+docker buildx ls
+
+docker buildx build -f dockerfile -t ${repo}/${image}:amd64 .
+docker push ${repo}/${image}:amd64
+
+#docker build --rm -f dockerfile -t ${image}:${version} .
+#docker tag ${image}:${version} ${repo}/${image}:${version} 
+#docker push ${repo}/${image}:${version}
 if [ $? -eq 0 ]; then
  echo "push Success"
 else 
